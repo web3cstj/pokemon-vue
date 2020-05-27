0d27a5e7a149e8a13251cbd96be045ef83658beb
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-05-27 19:48:43 -0400

    Installation Laravel VueJs Router

---
+++ resources/js/routes.js
@@ -0,0 +1,15 @@
+import Hello from './views/Hello'
+import Home from './views/Home'
+
+export default [
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/hello',
+        name: 'hello',
+        component: Hello,
+    },
+];

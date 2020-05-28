3f4c8ef2a0f97ac389fed129116b75801af738cb
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-05-28 09:28:09 -0400

    index fonctionnel

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', 'AppController@index');
+
+Route::group(['prefix'=>'pokemons', 'where'=>['pokemon'=>'[0-9]+']], function() {
+    Route::get('/',                     'PokemonController@index');
+    Route::get('/create',               'PokemonController@create');
+    Route::post('/create',              'PokemonController@store');
+    Route::get('/{pokemon}',            'PokemonController@show');
+    Route::get('/edit/{pokemon}',       'PokemonController@edit');
+    Route::post('/edit/{pokemon}',      'PokemonController@update');
+    Route::get('/delete/{pokemon}',     'PokemonController@delete');
+    Route::post('/delete/{pokemon}',    'PokemonController@destroy');
+});
